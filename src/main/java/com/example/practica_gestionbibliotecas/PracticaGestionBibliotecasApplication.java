670867f0818a25b888ca033119a50829fbc2eca1
rodri <rodrigomatamorosbecerra@gmail.com>
2023-11-20 12:25:54 +0100

    Initial commit

---
+++ src/main/java/com/example/practica_gestionbibliotecas/PracticaGestionBibliotecasApplication.java
@@ -0,0 +1,13 @@
+package com.example.practica_gestionbibliotecas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaGestionBibliotecasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaGestionBibliotecasApplication.class, args);
+	}
+
+}
